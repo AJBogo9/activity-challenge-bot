07acab549a9f3b8e790b60b6ba25acd7645a4e62
Andreas Bogossian <andreas.bogossian@aalto.fi>
2025-12-12 22:15:29 +0200

    modified code to use Metabase instead of self made script for visualization

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { connectDatabase, disconnectDatabase } = require('./database')
-const bot = require('./bot')
-const scheduleReminders = require('./utils/schedule-reminders')
+const { connectDatabase, disconnectDatabase } = require('./src/database')
+const bot = require('./src/bot')
+const scheduleReminders = require('./src/utils/schedule-reminders')
 
 async function handleError(error) {
   console.error('Unexpected error occurred:', error)
